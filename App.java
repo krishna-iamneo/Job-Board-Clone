10e5b04c1f0ef60134055d18a1d8f8355af242c9
shivam-neo <163996316+shivam-neo@users.noreply.github.com>
2024-03-23 15:36:09 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,13 +1,13 @@
 package com.job_board;
- 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
- 
-public class App {
 
+public class App {
     static final String DB_NAME = "JobNestDB";
+
     public static void main(String[] args) {
         Connection conn = null;
         Scanner scanner = new Scanner(System.in);
@@ -16,21 +16,26 @@ public class App {
             if (!DatabaseManager.databaseExists(DB_NAME)) {
                 DatabaseManager.createDatabase(DB_NAME);
             }
+
             // Connect to the database
             String dbUrl = "jdbc:mysql://localhost/" + DB_NAME;
             conn = DriverManager.getConnection(dbUrl, DatabaseManager.USER, DatabaseManager.PASS);
             System.out.println("Connected to database");
+
             // Create table if not exists
             TableCreator.createTableIfNotExists(conn);
+
             // Login system
             System.out.println("Welcome to Job Board!");
             System.out.print("Are you an admin? (yes/no): ");
             String userType = scanner.nextLine().toLowerCase();
+
             if (userType.equals("yes")) {
                 System.out.print("Enter username: ");
                 String username = scanner.nextLine();
                 System.out.print("Enter password: ");
                 String password = scanner.nextLine();
+
                 // Validate admin credentials
                 if (username.equals("admin") && password.equals("admin@123")) {
                     // Perform admin operations
@@ -38,7 +43,11 @@ public class App {
                 } else {
                     System.out.println("Invalid username or password. Access denied.");
                 }
+            } else {
+                // Perform user operations
+                UserOperations.performUserOperations(conn);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
