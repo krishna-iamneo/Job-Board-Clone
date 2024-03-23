22e0cb20ece85ecbf34137f4efa768a60fead655
krishna-iamneo <163994586+krishna-iamneo@users.noreply.github.com>
2024-03-23 12:25:13 +0530

    App.java
    Changes made due to the updating of table creation and database creation.

--- App.java
+++ App.java
@@ -1,1 +1,41 @@
-
+package com.job_board;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+ 
+public class App {
+    static final String DB_NAME = "JobNestDB";
+ 
+    public static void main(String[] args) {
+        Connection conn = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            // Check if the database exists, if not, create it
+            if (!DatabaseManager.databaseExists(DB_NAME)) {
+                DatabaseManager.createDatabase(DB_NAME);
+            }
+ 
+            // Connect to the database
+            String dbUrl = "jdbc:mysql://localhost/" + DB_NAME;
+            conn = DriverManager.getConnection(dbUrl, DatabaseManager.USER, DatabaseManager.PASS);
+            System.out.println("Connected to database");
+ 
+            // Create table if not exists
+            TableCreator.createTableIfNotExists(conn);
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+                scanner.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
